16de308664866e3edc4537b86e41e9ae30404e9e
mphoramodike08 <mramodike08@gmail.com>
2026-03-15 22:18:54 +0200

    Add garden advice function based on current month

---
+++ garden_advice.py
@@ -0,0 +1,15 @@
+import datetime
+
+def garden_advice():
+    month = datetime.datetime.now().month
+
+    if month in [12, 1, 2]:
+        print("Summer: Water plants frequently and protect from heat.")
+    elif month in [3, 4, 5]:
+        print("Autumn: Prepare soil and plant cool season vegetables.")
+    elif month in [6, 7, 8]:
+        print("Winter: Protect plants from frost.")
+    elif month in [9, 10, 11]:
+        print("Spring: Plant flowers and vegetables.")
+
+garden_advice()
